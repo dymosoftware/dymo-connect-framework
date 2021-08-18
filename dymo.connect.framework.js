8bd233996d29c691562f5afa7ffed4156410181d
giuliodamato <giulio.damato@gmail.com>
2021-08-18 18:51:04 +0200

    improved the regex for dymo.xml.serialize

--- dymo.connect.framework.js
+++ dymo.connect.framework.js
@@ -10129,7 +10129,7 @@ dymo.xml.parse = function (e) {
 ,
 dymo.xml.serialize = function (e) {
     function fix (e) {
-        return e.replaceAll(/<Color (.+)\/>/g, "<Color $1> </Color>");
+        return e.replaceAll(/<Color([^\/]+)\/>/g, "<Color$1> </Color>");
     }	
 
     return fix(goog.dom.xml.serialize(e))
